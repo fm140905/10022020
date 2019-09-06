fd77519eeaf2aafdfaf0a36362f79d3d38303b54
Ming Fang <mingf2@illinois.edu>
2019-09-06 17:36:13 +0000

    Upload New File

---
+++ source/readData.h
@@ -0,0 +1,29 @@
+#ifndef READDATA_H
+#define READDATA_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+#include "TFile.h"
+#include "events.h"
+#include "getInputParameters.h"
+//#include "output.h"
+
+Int_t readTimeStamp(const Parameters &setting,
+                    std::vector<std::vector<UShort_t>> &flags,
+                    std::vector<std::vector<ULong64_t>> &timeStamps);
+
+Int_t readCoincidences(const Parameters &setting,
+                const std::vector<std::vector<UInt_t>> &pairs,
+                std::vector< std::vector< Event > > &coincidentEvents);
+
+Int_t readEvents(const Parameters &setting,
+                std::vector<std::vector<Event>> &multichannelevents);
+// Int_t newPulseHeight(const Parameters &setting,
+//                 std::vector< std::vector< Event > > &coincidentEvents);
+
+#endif // READDATA_H
